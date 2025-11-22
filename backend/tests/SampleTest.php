b560e79a763e6601b82b4d2c8aeddcbdd303647c
Manish Kak <manish.kak@rediffmail.com>
2025-11-22 16:20:02 -0500

    update test case to fail+yml to run even with a failing test case, just for practice

--- backend/tests/SampleTest.php
+++ backend/tests/SampleTest.php
@@ -6,6 +6,6 @@ class SampleTest extends TestCase
 {
     public function test_addition()
     {
-        $this->assertEquals(4, 2 + 3);
+        $this->assertEquals(4, 2 + 3); // failing test case done on purpose, check ci-cd-demo.yml file
     }
 }
