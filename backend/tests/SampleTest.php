6b965b5a55540c0d93f695f09d39a71595afc678
Manish Kak <manish.kak@rediffmail.com>
2026-02-08 15:09:16 -0500

    making changes 02/08

--- backend/tests/SampleTest.php
+++ backend/tests/SampleTest.php
@@ -6,6 +6,7 @@ class SampleTest extends TestCase
 {
     public function test_addition()
     {
-        $this->assertEquals(4, 2 + 3); // failing test case done on purpose, check ci-cd-demo.yml file
+        $this->assertEquals(4, 2 + 2); // failing test case done on purpose, check ci-cd-demo.yml file
+        // testing some changes
     }
 }
